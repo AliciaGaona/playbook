c617968c973784bbf432357faf5fa3d7565d6b1f
AliciaGaona <aligaovela@gmail.com>
2022-04-22 22:24:57 -0500

    Agregrando segunda parte de ejercicio 1

--- weekly_mission_2/example1/example1_2.js
+++ weekly_mission_2/example1/example1_2.js
@@ -1,4 +1,20 @@
 console.log("2)creación de un objeto con propiedades")
 
-
-console.log(myObject)
+const Twiter ={
+    user: {
+        user: "Agv",
+        username: "Alicia Gaona Velázquez",
+        bio: "..."
+      },
+      trending_topic:"...",
+      hashtag:"#hi"
+}
+const Facebbok ={
+    user: "Alicia",
+    post:"...",
+    biography:"AliciaGaona"
+}
+const Uber={
+    profile:"Alicia",
+    travel:"Casa de Toño"
+}
